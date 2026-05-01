6fe6141707f68f4cf8c41b052f9a9b74b14da33c
김태경/AICC_Assistant개발팀 <taekyung@lguplus.co.kr>
2026-05-01 10:16:08 +0900

    git정보 변경2

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/seaking7/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('taekyung@lguplus.co.kr')
         }
